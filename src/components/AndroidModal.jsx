44dcc3ca8568f99e3ce7423d285ef99577f3f1ab
samuelsi9 <120906860+samuelsi9@users.noreply.github.com>
2026-04-30 19:36:07 +0300

    Update download link for Android app to APK on si9 github

--- src/components/AndroidModal.jsx
+++ src/components/AndroidModal.jsx
@@ -56,7 +56,7 @@ export default function AndroidModal({ open, onClose }) {
         </ol>
         {/* for testing purposes  //href="https://github.com/DeograciasGraole/land-ng-koliago/releases/download/v1.0/koliaGo.app.zip" */}
         <a
-          href="https://github.com/DeograciasGraole/land-ng-koliago/releases/download/v1.0/koliaGo.zip"
+          href="https://github.com/DeograciasGraole/land-ng-koliago/releases/download/v1.0/app-release.apk"
           download
           className="android-modal__download js-android-download-zip"
         >
